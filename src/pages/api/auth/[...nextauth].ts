db1d71b09e6c63cc10847167683b0baa1db7debd
Waschislin <wlalucena@gmail.com>
2022-11-04 15:57:22 -0300

    Salvando dados no faunaDB e verificando duplicidade

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github'
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,26 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
-})
+  callbacks: {
+    async signIn(user) {
+      const { user: email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(email.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email: email.email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email.email)))
+          )
+        );
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
